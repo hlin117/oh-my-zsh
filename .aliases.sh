ea09293ac2b82669d5d88563f4db7cb05e7f683e
hlin117 <hlin117@gmail.com>
2015-04-02 23:35:02 -0500

    removing stuff

--- .aliases.sh
+++ .aliases.sh
@@ -83,4 +83,7 @@ if [ "$USER" = "halin2" ] ; then
     if [ -d /class/cs225 ] ; then
         export PATH="/class/cs225/llvm/bin":$PATH
     fi
+#    else
+#        #echo "Warning: Clang is not loaded"
+#    fi
 fi
